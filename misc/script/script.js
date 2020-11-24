cd454022b802d7573789e149324deadc229f0d31
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2020-11-24 14:55:42 -0800

    first scripting file

---
+++ misc/script/script.js
@@ -0,0 +1,33 @@
+const courselist = [
+    {code:"ACIT 1620", name: "Web Fundamentals Technologies"}, 
+    {code:"ACIT 1515", name: "Introduction to Python"},
+    {code: "ACIT 1630", name: "Introduction to Databases"}
+]
+
+var answer = prompt ("Enter a 4 digit number: ");
+i=0;
+while (i< courselist.length){
+    let num =''
+    for(count = 0; count<courselist[i].code.length; count ++){
+        if (isNaN(courselist[i].code[count])==false)
+            num+=courselist[i].code[count]
+    }
+
+    if (answer.length!=4){
+        answer = prompt("Invalid data entered")
+        i=0
+    }
+    else if (num.trim() == answer){
+        alert(`Yes I am taking the course: ${courselist[i].code} ${courselist[i].name}`);
+        console.log("Successfully Added to the list")
+        break;}
+
+    else if (i == courselist.length-1){
+        courselist.push({code: answer, name: null});
+        break;}
+    i+=1}
+
+    
+
+    
+    
