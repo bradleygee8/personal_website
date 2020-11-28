a72153d8e275f0ccf34fac87bb9efc781738e6ab
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2020-11-28 00:01:01 -0800

    using querySelectors to get courses

--- misc/script/script.js
+++ misc/script/script.js
@@ -1,8 +1,20 @@
-const courselist = [
-    {code:"ACIT 1620", name: "Web Fundamentals Technologies"}, 
-    {code:"ACIT 1515", name: "Introduction to Python"},
-    {code: "ACIT 1630", name: "Introduction to Databases"}
-]
+var link = document.querySelectorAll("li.item");
+var p_tags = document.querySelectorAll("li.item p");
+var course_des = []; 
+var course_num = [];
+var date1 = [];
+for (i = 0 ; i < link.length; i++){
+    course_des.push(toString(link[i].querySelector('p')));
+    course_num.push(link[i].querySelector('a').text);
+    date1.push(p_tags[i+1])
+}
+for(i=0;i<date1.length;i++)
+    console.log(date1[i])
+const courselist = [];
+for (i=0 ; i <course_num.length;i++){
+    var temp = {code: course_num[i], name: course_des[i], date: date1[i]}
+    courselist.push(temp)
+}
 
 var answer = prompt ("Enter a 4 digit number: ");
 i=0;
@@ -19,15 +31,13 @@ while (i< courselist.length){
     }
     else if (num.trim() == answer){
         alert(`Yes I am taking the course: ${courselist[i].code} ${courselist[i].name}`);
+        document.querySelector('li.item').style.color = 'red'
         break;}
 
     else if (i == courselist.length-1){
-        courselist.push({code: answer, name: null});
+        courselist.push({code: answer, name: "N/A", date:"Fall2020"});
         console.log("Successfully Added to the list")
         break;}
     i+=1}
 
-    
 
-    
-    
